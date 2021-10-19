7e56e97b3c4959e3d0312edff35f50c6f5e5619d
Saba <saba.alikhani@ymail.com>
2021-10-19 19:38:23 +0330

    Added the related animation to the loading icon

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const div_button = document.querySelector("button > div");
 
 const downloading_state = document.querySelector(".downloading");
 
+const loading_icon = document.querySelector("#loading");
+
+const loading_arrow = document.querySelector("svg > symbol#loading > g");
+
+const done_icon = document.querySelector("#done");
+
 button.addEventListener("click", clickHandler);
 
 function clickHandler() {
@@ -20,10 +26,14 @@ function clickHandler() {
     hit.remove();
     div_button.className = "downloading";
     span_button.textContent = "Loading";
+    span_button.style.left = "7rem";
+    loading_icon.style.display = "inline";
+    loading_arrow.style.animation = "loading-arrow 1s ease-in-out infinite";
     span_button.style.color = "#ffffff";
   }, 1000);
 
   setTimeout(() => {
+    loading_icon.style.display = "none";
     div_button.classList.remove("downloading");
     div_button.className = "done";
 
